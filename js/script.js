5246331c31bfc31dc6aa389057b2d22031c2ac5e
wayne mcnaught <wayne.mcnaught@gmail.com>
2023-11-22 20:07:12 +0000

    Initial Upload and Site Creation

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//accordian drop downs
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active1");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+//adds currant date to footers
+var date = new Date();
+document.getElementById("js-year").innerHTML = date.getFullYear();
